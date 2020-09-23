539c7572bc65b2e93e0c74ab5e472d89d69562cf
abi franklin <abifranklin@gmail.com>
2020-09-23 12:55:53 -0500

    Restart

--- dist/scripts/enums.js
+++
@@ -1,6 +0,0 @@
-export var AccountType;
-(function (AccountType) {
-    AccountType[AccountType["Checking"] = 0] = "Checking";
-    AccountType[AccountType["Savings"] = 1] = "Savings";
-})(AccountType || (AccountType = {}));
-//# sourceMappingURL=enums.js.map
--- src/scripts/checking-account.ts
+++
@@ -1,9 +0,0 @@
-import { BankAccount } from "./bank-account";
-import { AccountType } from "./enums";
-
-export class CheckingAccount extends BankAccount {
-  accountType = AccountType.Checking;
-  getAccountInfo() {
-      return {};
-  }
-}
